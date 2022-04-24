f35fcea8a020488cf38e6e7278bff6f491f673a9
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-04-24 21:17:39 +0600

    scrope

---
+++ Python/scrope.py
@@ -0,0 +1,16 @@
+# l = 10  #global scope
+# def function1(n):
+#     # l = 5 
+#     m = 8 # local scope
+#     global l  # permission and change global variable
+#     l = l + 45
+#     print(l,m)
+#     print(n,"i have printed")
+
+# function1("this is me")    
+
+
+
+
+
+
